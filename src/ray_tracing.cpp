c3b87abbabef412e5c603ac22a367610de329958
mdoolaar <matthijsdoolaar@live.nl>
2020-10-21 11:51:16 +0200

    Fix lighting on other side of plane

--- src/ray_tracing.cpp
+++ src/ray_tracing.cpp
@@ -63,7 +63,10 @@ bool intersectRayWithTriangle(const glm::vec3& v0, const glm::vec3& v1, const gl
         if (!inTriangle)
             ray.t = originalT;
         else {
-            hitInfo.normal = plane.normal;
+            if (glm::dot(glm::normalize(ray.direction), glm::normalize(plane.normal)) < 0)
+                hitInfo.normal = plane.normal;
+            else
+                hitInfo.normal = -plane.normal;
             return true;
         }
     }
